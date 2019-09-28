5915ead2199bf3f5356305aa58c58d1e4e19fff1
Sai harika paluri <harikapaluri@gmail.com>
2019-09-28 12:52:35 -0400

    Server.py
    Changed all function names, variables and added comments

--- Server.py
+++ Server.py
@@ -1,57 +1,65 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-
+#Function for accepting incoming client connections
 def incoming():
+    #A loop that waits forever till it gets a client connection
     while True:
-        client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Welcome to SHH Chat server, now enter your name and join!", "utf8"))
-        Thread(target=new_client, args=(client,client_address)).start()
+        server,clientList = SERVER.accept()
+        print("%s:%s has connected." % clientList)
+        server.send(bytes("Welcome to SHH Chat server, enter your name and join the room!", "utf8"))
+        Thread(target=new_client, args=(server,clientList)).start()
 
-
-def new_client(client,address): 
-
-    name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type !q to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
+#Function for connecting with a new client inorder to recieve and send messages to and from server
+def new_client(newclient,address): 
+    #Client enters name which has to be recieved by server. 
+    nameOfClient = newclient.recv(BUFSIZ).decode("utf8")
+    #Welcome message from the server
+    welcome = 'Hello %s! If you ever want to quit , type !q to log out of the chat room.' % nameOfClient
+    #Server sending a welcome message to the client
+    newclient.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the room!" % nameOfClient
+    #Sending a broadcast message that is visible to all the clients conected over the chat room.
+    messageBroadcast(bytes(msg, "utf8"))
+    #Saving all client names in a dictionary
+    listOfClients[newclient] = nameOfClient
 
     while True:
-        msg = client.recv(BUFSIZ)
+        msg = newclient.recv(BUFSIZ)
         if msg != bytes("!q", "utf8"):
-            broadcast(msg, "->"+name+": ")
+            messageBroadcast(msg, "->"+nameOfClient+": ")
         else:
-            client.send(bytes("!q", "utf8"))
-            client.close()
-            del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
-            print("%s:%s has discconnected." % address)
+            newclient.send(bytes("!q", "utf8"))     #New client is sedning !q to the server inorder to quit the chat room.
+            newclient.close()
+            del listOfClients[newclient]          #Closing connection for new client and deleting name entry from clients dictionary.
+            messageBroadcast(bytes("%s has left the chat room." % nameOfClient, "utf8"))
+            print("%s:%s has logged out ." % address)
             break
 
 
-def broadcast(msg, prefix=""):  
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+#Function for Broadcast message that has to be sent to all existing Client connections of the server.
+def messageBroadcast(message, nameId=""):  
+    for socketConn in listOfClients:
+        socketConn.send(bytes(nameId, "utf8")+message)
 
-        
-clients = {}
-addresses = {}
+ #A Dictionary of client names       
+listOfClients = {}
+#A Dictionary of client IP's
+listOfAddresses = {}
 
 HOST = ''
 PORT = int(input("Enter port number: "))
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
-
 SERVER = socket(AF_INET, SOCK_STREAM)
+#Binding host address to open a socket connection
 SERVER.bind(ADDR)
 
 if __name__ == "__main__":
+    #Server listening for client requests here we have mentioned atmost 5 requests.
     SERVER.listen(5)
-    print("Accepting Connections...")
+    print("Listening for incoming connections....")
     ACCEPT = Thread(target=incoming)
-    ACCEPT.start()
-    ACCEPT.join()
+    ACCEPT.start() #Start of the main thread which starts the infinite loop.
+    ACCEPT.join()  #The main thread wait's till the child thread that is Accept completes its execution.
     SERVER.close()
