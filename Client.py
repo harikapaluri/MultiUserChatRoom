a2311f5fd11cc83db0b2db8e5c2cc610876a5fda
Harika Paluri <harikapaluri@gmail.com>
2019-09-29 14:37:28 -0400

    Update Client.py
    Function names,comments and variable names

--- Client.py
+++ Client.py
@@ -1,33 +1,31 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-
-client_socket = socket(AF_INET, SOCK_STREAM)
-
-HOST = input('Enter host: ')
+#Opening a socket connection from client side
+clientConnection = socket(AF_INET, SOCK_STREAM)
 PORT = int(input('Enter port: '))
-
-BUFSIZ = 1024
+HOST = input('Enter host: ')
+#Buffer Size
+BufferSize = 1024
 ADDR = (HOST, PORT)
-client_socket.connect(ADDR)
-
-def receive():
-    """Handles receiving of messages."""
-    while True:
+#Sending a request to the server to connect
+clientConnection.connect(ADDR)
+#Send Function call for the client to send messages on the server.
+def sendMessages(): 
+     sendMessage=input()
+     clientConnection.send(bytes(sendMessage, "utf8"))
+     if sendMessage == "!q":         
+        clientConnection.close()
+        exit()
+     sendMessages()
+#Recieve function  call for the client to recieve messages from other clients connected to the server.
+def recvMessages():
+     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
-            print(msg)
-        except OSError:  # Possibly client has left the chat.
+            messageRecieved = clientConnection.recv(BufferSize ).decode("utf8")
+            print(messageRecieved)
+        except OSError:  #Chance of an excpetion arrises  if  the client has decided to close his connection. 
             break
 
-def send():  # event is passed by binders.
-    """Handles sending of messages."""
-    msg=input()
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "!q":
-        client_socket.close()
-        exit()
-    send()
-
 if __name__ == '__main__':
-    Thread(target = receive).start()
-    Thread(target = send).start()
+    Thread(target = recvMessages).start()
+    Thread(target = sendMessages).start()
